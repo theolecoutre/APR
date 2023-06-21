3d4167d3c057d628ffe34fe65578800dc985dd57
Théo Lecoutre <theolecoutre@gmail.com>
2023-06-21 14:58:19 +0200

    Second commit

--- annotation_converter.py
+++ annotation_converter.py
@@ -3,29 +3,37 @@ import xml
 import xml.etree.ElementTree as ET
 
 
-mytree = ET.parse('ICDAR_2003/SceneTrialTrain/locations.xml')
+mytree = ET.parse('SceneTrialTrain/locations_simple.xml')
 myroot = mytree.getroot()
 
-for r, d, f in os.walk("ICDAR_2003/SceneTrialTrain/apanar_06.08.2002/"):
+# for image in myroot.findall("image/imageName"):
+     
+#      image.text = image.text.split("/")[1]
+
+# mytree.write("test.xml")
+
+
+for r, d, f in os.walk("SceneTrialTrain/"):
         for file in f:
-            try:
-                image = myroot.findall(f"image[imageName='apanar_06.08.2002/{file}']")[0]
+            if file.endswith(".jpg"):
+                try:
+                    image = myroot.findall(f"image[imageName='{file}']")[0]
 
-                resolution = image.find("resolution")
-                totalWidth = int(resolution.attrib["x"])
-                totalHeight = int(resolution.attrib["y"])
+                    resolution = image.find("resolution")
+                    totalWidth = int(resolution.attrib["x"])
+                    totalHeight = int(resolution.attrib["y"])
 
-                coordinates = image.find("taggedRectangles/taggedRectangle")
+                    coordinates = image.find("taggedRectangles/taggedRectangle")
 
-                x = int(coordinates.attrib["x"].split(".")[0])/totalWidth
-                y = int(coordinates.attrib["y"].split(".")[0])/totalHeight
-                width = int(coordinates.attrib["width"].split(".")[0])/totalWidth
-                height = int(coordinates.attrib["height"].split(".")[0])/totalHeight
+                    x = int(coordinates.attrib["x"].split(".")[0])/totalWidth
+                    y = int(coordinates.attrib["y"].split(".")[0])/totalHeight
+                    width = int(coordinates.attrib["width"].split(".")[0])/totalWidth
+                    height = int(coordinates.attrib["height"].split(".")[0])/totalHeight
 
-                with open(file[0:-4] + ".txt", "w") as textFile:
+                    with open(f"SceneTrialTrain/{file[0:-4]}" + ".txt", "w") as textFile:
 
-                    textFile.write(f"{x} {y} {width} {height}")
+                        textFile.write(f"{x} {y} {width} {height}")
 
-                print(file)
-            except Exception as err:
-                 print("error")
+                    print(file)
+                except Exception as err:
+                    print("error")
